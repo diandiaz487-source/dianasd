cc77f64de16cc000701fe62e2cd4b590894eb995
Diana Sotelo Díaz <diandiaz487@gmail.com>
2026-04-17 17:47:47 -0600

    Actualización de db y login

--- db.php
+++ db.php
@@ -4,9 +4,9 @@
 
 function conectarDB() {
     $host = "localhost";
-    $db   = "jreynoso_db";
-    $user = "jreynoso";
-    $pass = "12345678";
+    $db   = "dsotelo_db";
+    $user = "dsotelo";
+    $pass = "123456789";
     $charset = "utf8mb4";
 
     // El DSN (Data Source Name) define el tipo de driver y los datos del servidor
